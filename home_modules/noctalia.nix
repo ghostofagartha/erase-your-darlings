dd67531150fb0cac7183ae6066aac6184fa7ba7a
ghostofagartha <darkside4464@gmail.com>
2026-08-06 17:21:14 +0500

    Added transition oon startup

--- home_modules/noctalia.nix
+++ home_modules/noctalia.nix
@@ -165,6 +165,7 @@
 
       wallpaper = {
         enabled = true;
+        transition_on_startup = true;
         default = {
           path = "/home/aori/Pictures/Wallpapers/Bike_Wallpaper24.jpg";
         };
