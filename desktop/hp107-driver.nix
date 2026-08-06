ed28af1c42c01bd53fdb68590eebfb2d1fd022d0
ghostofagartha <darkside4464@gmail.com>
2026-08-06 21:07:46 +0500

    Added cups

--- desktop/hp107-driver.nix
+++ desktop/hp107-driver.nix
@@ -1,8 +1,4 @@
-{ lib
-, stdenv
-, autoPatchelfHook
-, cups
-}:
+{ lib, stdenv, autoPatchelfHook, cups }:
 
 stdenv.mkDerivation {
   pname = "hp-laser-10x-driver";
@@ -25,28 +21,18 @@ stdenv.mkDerivation {
     mkdir -p $out/lib/cups/filter
     mkdir -p $out/share/cups/model
 
-    # HP SPL raster filter
     install -m755 \
       uld/x86_64/rastertospl \
       $out/lib/cups/filter/rastertospl
 
-    # HP printer PPD
     install -m644 \
       uld/noarch/share/ppd/HP_Laser_10x_Series.ppd \
       $out/share/cups/model/HP_Laser_10x_Series.ppd
-
-    # Make the old HP PPD compatible with modern CUPS
-    sed -i '/^\*cupsFilter:/d' \
-      $out/share/cups/model/HP_Laser_10x_Series.ppd
-
-    echo '*cupsFilter2: "application/vnd.cups-raster application/vnd.cups-raster 0 rastertospl"' \
-      >> $out/share/cups/model/HP_Laser_10x_Series.ppd
   '';
 
   meta = {
-    description = "HP Laser 103/107/108 Linux driver";
-    homepage = "https://support.hp.com";
-    license = lib.licenses.unfree;
+    description = "HP Laser 10x Series Linux driver";
     platforms = [ "x86_64-linux" ];
+    license = lib.licenses.unfree;
   };
 }
