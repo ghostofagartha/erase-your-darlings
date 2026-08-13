9ea489356d94d95ff264dafbfe75d0cca88f3362
ghostofagartha <darkside4464@gmail.com>
2026-08-13 05:50:25 +0500

    Undows

--- system/power.nix
+++ system/power.nix
@@ -2,34 +2,33 @@
 
 {
   # --- TLP (AC/battery power profiles) ---
-  # services.tlp = {
-  #   enable = true;
-  #   pd.enable = true;
-  #
-  #   settings = {
-  #     CPU_SCALING_GOVERNOR_ON_AC = "performance";
-  #     CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
-  #     CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
-  #     CPU_ENERGY_PERF_POLICY_ON_BAT = "power";
-  #
-  #     CPU_BOOST_ON_AC = 1;
-  #     CPU_BOOST_ON_BAT = 0;
-  #     START_CHARGE_THRESH_BAT0 = 75;
-  #     STOP_CHARGE_THRESH_BAT0 = 80;
-  #
-  #     PCIE_ASPM_ON_AC = "performance";
-  #     PCIE_ASPM_ON_BAT = "powersave";
-  #
-  #     SOUND_POWER_SAVE_ON_AC = 0;
-  #     SOUND_POWER_SAVE_ON_BAT = 1;
-  #
-  #     USB_AUTOSUSPEND = 1;
-  #
-  #     WIFI_PWR_ON_AC = "off";
-  #     WIFI_PWR_ON_BAT = "on";
-  #   };
-  # };
-  services.power-profiles-daemon.enable = true;
+  services.tlp = {
+    enable = true;
+    pd.enable = true;
+
+    settings = {
+      CPU_SCALING_GOVERNOR_ON_AC = "performance";
+      CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
+      CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
+      CPU_ENERGY_PERF_POLICY_ON_BAT = "power";
+
+      CPU_BOOST_ON_AC = 1;
+      CPU_BOOST_ON_BAT = 0;
+      START_CHARGE_THRESH_BAT0 = 75;
+      STOP_CHARGE_THRESH_BAT0 = 80;
+
+      PCIE_ASPM_ON_AC = "performance";
+      PCIE_ASPM_ON_BAT = "powersave";
+
+      SOUND_POWER_SAVE_ON_AC = 0;
+      SOUND_POWER_SAVE_ON_BAT = 1;
+
+      USB_AUTOSUSPEND = 1;
+
+      WIFI_PWR_ON_AC = "off";
+      WIFI_PWR_ON_BAT = "on";
+    };
+  };
 
   # --- Swap / zram ---
   zramSwap = {
