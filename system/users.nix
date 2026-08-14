5d3ed4126fd98e98960a2408630350146847c92d
ghostofagartha <darkside4464@gmail.com>
2026-08-15 02:23:10 +0500

    Set up zsh again

--- system/users.nix
+++ system/users.nix
@@ -9,10 +9,10 @@
     uid = 1000;
     extraGroups = [ "wheel" "networkmanager" "render" "video" ];
     hashedPassword = "$6$jMy.J1W64oPNZ6WG$ivJyoDSxPVtlKiUZ7/CDSgxdR0yjSryW1UXWeoCnIhxF.col/RCBAnbA18xd5Oa1ExP33jWhuH5kJ.b0wFHW81";
-    shell = pkgs.fish;
+    shell = pkgs.zsh;
   };
 
-  programs.fish.enable = true;
+  programs.zsh.enable = true;
 
   programs.git = {
     enable = true;
