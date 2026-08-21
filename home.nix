8ee3b5d96b331e73d703a23241138ffb0d10a7ca
ghostofagartha <darkside4464@gmail.com>
2026-08-21 22:07:19 +0500

    Changed shell

--- home.nix
+++ home.nix
@@ -7,7 +7,6 @@
     ./home/mpv.nix
     ./home/nvim.nix
     ./home/cursor.nix
-    ./home/shell.nix
     ./home/noctalia.nix
     ./home/packages.nix
   ];
--- system/users.nix
+++ system/users.nix
@@ -9,10 +9,7 @@
     uid = 1000;
     extraGroups = [ "wheel" "networkmanager" "render" "video" ];
     hashedPassword = "$6$jMy.J1W64oPNZ6WG$ivJyoDSxPVtlKiUZ7/CDSgxdR0yjSryW1UXWeoCnIhxF.col/RCBAnbA18xd5Oa1ExP33jWhuH5kJ.b0wFHW81";
-    shell = pkgs.zsh;
   };
-
-  programs.zsh.enable = true;
 
   programs.git = {
     enable = true;
