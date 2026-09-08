6084b6ff8a7b6e590dd73baa2924394274ef592d
ghostofagartha <darkside4464@gmail.com>
2026-09-08 09:33:30 +0500

    Configured nvim

--- home/nvim.nix
+++ home/nvim.nix
@@ -1,12 +1,21 @@
 { inputs, ... }:
 
 {
-  # LazyVim
   programs.lazyvim = {
     enable = true;
+
     extras = {
       lang.nix.enable = true;
     };
+
+    config = {
+      options = ''
+        -- Nix manages the plugins, so lazy.nvim should not
+        -- check for plugin updates at runtime.
+        require("lazy.core.config").options.checker.enabled = false
+      '';
+    };
+
     plugins = {
       colorscheme = ''
         return {
@@ -14,10 +23,12 @@
           name = "catppuccin",
           lazy = false,
           priority = 1000,
+
           opts = {
-            flavour = "mocha", -- "transparent" is not a valid flavour
+            flavour = "mocha",
             transparent_background = true,
             term_colors = true,
+
             integrations = {
               cmp = true,
               gitsigns = true,
@@ -25,20 +36,39 @@
               treesitter = true,
             },
           },
+
           config = function(_, opts)
             require("catppuccin").setup(opts)
-            -- Force transparency for UI elements that the plugin might miss
+
             vim.api.nvim_create_autocmd("ColorScheme", {
               pattern = "catppuccin",
               callback = function()
-                vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-                vim.api.nvim_set_hl(0, "NormalNC", { bg = "none" })
-                vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
-                vim.api.nvim_set_hl(0, "FloatBorder", { bg = "none" })
-                vim.api.nvim_set_hl(0, "SignColumn", { bg = "none" })
-                vim.api.nvim_set_hl(0, "LineNr", { bg = "none" })
+                vim.api.nvim_set_hl(0, "Normal", {
+                  bg = "none",
+                })
+
+                vim.api.nvim_set_hl(0, "NormalNC", {
+                  bg = "none",
+                })
+
+                vim.api.nvim_set_hl(0, "NormalFloat", {
+                  bg = "none",
+                })
+
+                vim.api.nvim_set_hl(0, "FloatBorder", {
+                  bg = "none",
+                })
+
+                vim.api.nvim_set_hl(0, "SignColumn", {
+                  bg = "none",
+                })
+
+                vim.api.nvim_set_hl(0, "LineNr", {
+                  bg = "none",
+                })
               end,
             })
+
             vim.cmd.colorscheme("catppuccin")
           end,
         }
