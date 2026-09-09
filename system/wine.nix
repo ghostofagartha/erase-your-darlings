5e4476fba3e709f8bf3fdf7e2fb6e5c4ed38bbea
ghostofagartha <darkside4464@gmail.com>
2026-09-09 20:16:43 +0500

    Added wine and enabled nvim

--- system/wine.nix
+++ system/wine.nix
@@ -6,9 +6,6 @@
     winetricks
   ];
 
-  hardware.graphics.enable32Bit = true;
-
-  # This moves the default Wine folder out of your home root into .local/share
   environment.sessionVariables = {
     WINEPREFIX = "$HOME/.local/share/wineprefixes/default";
   };
